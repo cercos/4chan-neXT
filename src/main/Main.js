2932e9c86cba4ec93bfa1e8b0dae56b641f40cbe
Johnny <877480+saxamaphone69@users.noreply.github.com>
2026-05-31 14:22:37 +1000

    Add discernible class to root for targeted styling

--- src/main/Main.js
+++ src/main/Main.js
@@ -135,7 +135,7 @@ var Main = {
     // Detect multiple copies of 4chan X
     if (doc && $.hasClass(doc, 'fourchan-x')) { return; }
     $.asap(docSet, function() {
-      $.addClass(doc, 'fourchan-xt', 'fourchan-x', 'seaweedchan');
+      $.addClass(doc, 'fourchan-next', 'fourchan-xt', 'fourchan-x', 'seaweedchan');
       if ($.engine) $.addClass(doc, `ua-${$.engine}`);
       BoardConfig.ready(() => {
         if (g.BOARD?.config.ws_board != null) $.addClass(doc, g.BOARD.config.ws_board ? 'ws' : 'nws');
